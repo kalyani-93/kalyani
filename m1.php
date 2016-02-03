7244f5f17f35190a4268de0e916189160597da42
kalyani <kalyani.ameex@gmail.com>
2016-02-03 18:13:38 +0530

    first commit

---
+++ m1.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<script>
+function showUser(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else { 
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+            }
+        };
+        xmlhttp.open("GET","m2.php?q="+str,true);
+        xmlhttp.send();
+    }
+}
+</script>
+</head>
+<body>
+
+<form>
+<input type="text" name="users" onkeyup="showUser(this.value)">
+
+</form>
+<br>
+<p id="txtHint"><b>Person info will be listed here...</b></p>
+
+</body>
+</html>
